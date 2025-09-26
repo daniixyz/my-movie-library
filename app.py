fd847f31b47e7bfb2d49f846200869af5f830c20
Daniella Machado <seu-email@exemplo.com>
2025-09-26 12:32:48 -0300

    site almost complete added templates of movie details and index fixed

--- app.py
+++ app.py
@@ -13,6 +13,24 @@ def index():
     
     return render_template("index.html", movies=movies)
 
+@app.get("/movie/<int:movie_id>")
+def movie_details(movie_id):
+    found_movie = None
+
+    with open('movies.csv', mode='r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        for movie in csv_reader:
+            if int(movie['id']) == movie_id:
+                found_movie = movie
+                break 
+
+    if found_movie:
+        return render_template("movie-details.html", movie=found_movie)
+
+    else:
+        return "Movie not found!", 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
