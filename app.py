78e19ef57140d26fc504d35ab27f14ecf219a39d
Daniella Machado <seu-email@exemplo.com>
2025-09-26 12:47:09 -0300

    add review fixed

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import csv
+from datetime import datetime
+
+
 
 app = Flask(__name__)
 
@@ -31,6 +34,56 @@ def movie_details(movie_id):
     else:
         return "Movie not found!", 404
 
+with open("movies.csv", mode="r", encoding="utf-8") as csv_file:
+    reader = list(csv.reader(csv_file))
+    last_id = 0
+
+    if len(reader) > 1:
+        last_row = reader[-1]
+        
+        if last_row:  
+            last_id = int(last_row[0]) 
+
+    next_id = last_id + 1 
+
+
+@app.get("/add-review")
+def show_add_form():
+    return render_template("add-review.html")
+
+@app.post("/add-review")
+def add_review():
+
+    with open("movies.csv", mode="r", encoding="utf-8") as csv_file:
+        reader = list(csv.reader(csv_file))
+        last_id = 0
+
+        if len(reader) > 1:
+            last_row = reader[-1]
+            if last_row:
+                last_id = int(last_row[0])
+
+        next_id = last_id + 1
+
+    watched_date_raw = request.form.get("watched_date")
+    date_object = datetime.strptime(watched_date_raw, "%Y-%m-%d")
+
+    formatted_date = date_object.strftime("%m/%d/%Y")
+
+    new_movie_data = [
+        next_id,
+        request.form.get("title"),
+        request.form.get("poster_url"),
+        request.form.get("rating"),
+        formatted_date,
+        request.form.get("review"),
+    ]
+
+    with open("movies.csv", mode="a", encoding="utf-8", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(new_movie_data)
+
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
